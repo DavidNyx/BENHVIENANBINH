4f2938db2d86c6e57fe2a922dd77a5812b6224c5
binhhieutu <63572337+binhhieutu@users.noreply.github.com>
2022-04-21 19:45:15 +0700

    Update BENHVIENANBINH1.sql
    change loai goi vacxin from bit into int

--- BENHVIENANBINH1.sql
+++ BENHVIENANBINH1.sql
@@ -67,7 +67,7 @@ CREATE TABLE GOIVACXIN
 (
 	MA_GVX CHAR(5),
 	TEN_GVX NVARCHAR(30) NOT NULL,
-	LOAI_GVX BIT NOT NULL, --0 là tiêm lẻ, 1 là gói tiêm, 2 là tiêm bắt buộc theo gói
+	LOAI_GVX INT NOT NULL, --0 là tiêm lẻ, 1 là gói tiêm, 2 là tiêm bắt buộc theo gói
 	NGAYSX DATE,
 	NGAYHH DATE,
 	DOITUONGTIEM NVARCHAR(100),
